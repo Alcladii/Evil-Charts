c0b6f18b0616f8a388c03251e2b73c1c48c9da29
Alcladii <122701312+Alcladii@users.noreply.github.com>
2025-08-24 22:51:22 +0100

    hatched bar chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
 import { DefaultBarChart } from "@/components/ui/default-bar-chart";
 import { DefaultMultipleBarChart } from "@/components/ui/default-multiple-bar-chart";
+import { HatchedBarChart } from "@/components/ui/hatched-bar-chart";
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center">
-      <div className="w-96 h-80">
+      <div className="w-96 h-80 p-2">
         <DefaultBarChart />     
       </div>
-      <div className="w-96 h-80">
+      <div className="w-96 h-80 p-2">
         <DefaultMultipleBarChart />
+      </div>
+      <div className="w-96 h-80 p-2">
+        <HatchedBarChart/>
       </div>
     </div>
   );
